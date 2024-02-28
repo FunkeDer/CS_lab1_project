1b9074b03d3806fc694671150cbaf071076c290c
FunkeDer <110049102+FunkeDer@users.noreply.github.com>
2024-02-28 10:34:00 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Lab1_CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(254, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вибрати текстовий файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(254, 352);
+            this.richTextBox1.TabIndex = 15;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Текст файлу:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(302, 166);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(272, 299);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(298, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Click += new System.EventHandler(this.label5_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(298, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Click += new System.EventHandler(this.label6_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(298, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Частота:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(457, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 20);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "0";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(297, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Кількість символів:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(698, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 43);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Вибрати файл (Base64)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(698, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(424, 352);
+            this.richTextBox2.TabIndex = 24;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(983, 420);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 43);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Зберегти файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1147, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 512);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Lab1_CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView(); // Ініціалізація DataGridVie
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Додаємо стовпці у DataGridView
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].Name = "Символ";
+            dataGridView1.Columns[1].Name = "Частота";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+           openFileDialog1.Filter = "Текстові файли (*.txt)|*.txt|Всі файли (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = openFileDialog1.FileName;
+                    string text = System.IO.File.ReadAllText(filePath);
+
+                    richTextBox1.Text = text; 
+
+                    Dictionary<char, double> frequencies = CalculateRelativeFrequencies(text);
+                    double entropy = calcAvgEntropy(frequencies);
+                    double informationContent = entropy * text.Length;
+
+                    long fileSizeInBytes = CalculateFileSizeInBytes(filePath);
+
+                    dataGridView1.Rows.Clear(); 
+
+                    foreach (var kvp in frequencies)
+                    {
+                        dataGridView1.Rows.Add(kvp.Key, kvp.Value); 
+                    }
+
+                    label5.Text = $"Ентропія: {entropy}";
+                    label6.Text = $"Кількість інформації: {calcAvgEntropy(frequencies) * CalculateTotalCharactersCount(text) / 8.00} байт";
+                    label8.Text = $"{CalculateTotalCharactersCount(text)}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Сталася помилка: {ex.Message}");
+                }
+            }
+        }
+
+        private long CalculateFileSizeInBytes(string filePath)
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            long fileSizeInBytes = fileInfo.Length;
+
+            return fileSizeInBytes;
+        }
+
+        private Dictionary<char, double> CalculateRelativeFrequencies(string text)
+        {
+            SortedDictionary<char, double> frequencies = new SortedDictionary<char, double>();
+
+            int totalSymbols = 0;
+
+            text = text.ToLower();
+
+            foreach (char c in text)
+            {
+                if (!Char.IsWhiteSpace(c))
+                {
+                    if (frequencies.ContainsKey(c))
+                    {
+                        frequencies[c]++;
+                    }
+                    else
+                    {
+                        frequencies[c] = 1;
+                    }
+
+                    totalSymbols++;
+                }
+            }
+            Dictionary<char, double> relativeFrequencies = new Dictionary<char, double>();
+
+            foreach (var kvp in frequencies)
+            {
+                double relativeFrequency = (double)kvp.Value / totalSymbols;
+                relativeFrequencies.Add(kvp.Key, relativeFrequency);
+            }
+
+            return relativeFrequencies;
+        }
+
+        double calcAvgEntropy(Dictionary<char, double> relativeFrequencies)
+        {
+            double result = 0;
+            foreach (var elem in relativeFrequencies)
+            {
+                if (1 / elem.Value != 0)
+                    result += elem.Value * Math.Log(1 / elem.Value, 2);
+            }
+
+            return result;
+        }
+
+        private int CalculateTotalCharactersCount(string text)
+        {
+            SortedDictionary<char, int> analText = new SortedDictionary<char, int>();
+            int letterCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!analText.ContainsKey(Char.ToLower(text[i])) && (Char.IsLetter(text[i]) || (Char.IsLetterOrDigit(text[i]))))
+                {
+                    analText.Add(Char.ToLower(text[i]), 1);
+                    letterCount++;
+                }
+                else if (analText.ContainsKey(Char.ToLower(text[i])))
+                {
+                    analText[Char.ToLower(text[i])]++;
+                    letterCount++;
+                }
+            }
+
+            return letterCount;
+        }
+
+        private static readonly char[] Base64Letters = new[]
+                                        {
+                                              'A'
+                                            , 'B'
+                                            , 'C'
+                                            , 'D'
+                                            , 'E'
+                                            , 'F'
+                                            , 'G'
+                                            , 'H'
+                                            , 'I'
+                                            , 'J'
+                                            , 'K'
+                                            , 'L'
+                                            , 'M'
+                                            , 'N'
+                                            , 'O'
+                                            , 'P'
+                                            , 'Q'
+                                            , 'R'
+                                            , 'S'
+                                            , 'T'
+                                            , 'U'
+                                            , 'V'
+                                            , 'W'
+                                            , 'X'
+                                            , 'Y'
+                                            , 'Z'
+                                            , 'a'
+                                            , 'b'
+                                            , 'c'
+                                            , 'd'
+                                            , 'e'
+                                            , 'f'
+                                            , 'g'
+                                            , 'h'
+                                            , 'i'
+                                            , 'j'
+                                            , 'k'
+                                            , 'l'
+                                            , 'm'
+                                            , 'n'
+                                            , 'o'
+                                            , 'p'
+                                            , 'q'
+                                            , 'r'
+                                            , 's'
+                                            , 't'
+                                            , 'u'
+                                            , 'v'
+                                            , 'w'
+                                            , 'x'
+                                            , 'y'
+                                            , 'z'
+                                            , '0'
+                                            , '1'
+                                            , '2'
+                                            , '3'
+                                            , '4'
+                                            , '5'
+                                            , '6'
+                                            , '7'
+                                            , '8'
+                                            , '9'
+                                            , '+'
+                                            , '/'
+                                        };
+
+        public static BitArray Append(BitArray current, BitArray after)
+        {
+            var bools = new bool[current.Count + after.Count];
+            current.CopyTo(bools, 0);
+            after.CopyTo(bools, current.Count);
+            return new BitArray(bools);
+        }
+
+        public void Reverse(ref BitArray array)
+        {
+            int length = array.Length;
+            int mid = (length / 2);
+
+            for (int i = 0; i < mid; i++)
+            {
+                bool bit = array[i];
+                array[i] = array[length - i - 1];
+                array[length - i - 1] = bit;
+            }
+        }
+
+        string Encode3bytes(byte[] bytes)
+        {
+            string result = "";
+
+            BitArray oneArr = new BitArray(new byte[] { bytes[0] });
+            Reverse(ref oneArr);
+
+            BitArray secondArr = new BitArray(new byte[] { bytes[1] });
+            Reverse(ref secondArr);
+
+            BitArray thirdArr = new BitArray(new byte[] { bytes[2] });
+
+            Reverse(ref thirdArr);
+
+            BitArray bitArray = new BitArray(0);
+            bitArray = Append(oneArr, secondArr);
+            bitArray = Append(bitArray, thirdArr);
+
+
+            BitArray tmpBit = new BitArray(6);
+            int j = 0;
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                tmpBit.Set(i - 6 * j, bitArray[i]);
+                if (((i + 1) % 6 == 0 || i == 23) && i != 0)
+                {
+                    byte[] tmpbyte = new byte[1];
+                    tmpBit.CopyTo(tmpbyte, 0);
+                    int value = 0;
+                    Reverse(ref tmpBit);
+                    for (int k = 0; k < tmpBit.Count; k++)
+                    {
+                        if (tmpBit[k])
+                            value += Convert.ToInt16(Math.Pow(2, k));
+                    }
+                    result += Base64Letters[value];
+                    j++;
+
+                    if (i / 8 >= 1 && bytes[1] == 0 && bytes[2] == 0)
+                    {
+                        result += "==";
+                        return result;
+                    }
+                    if (i / 8 >= 2 && bytes[2] == 0)
+                    {
+                        result += "=";
+                        return result;
+                    }
+
+                }
+
+
+            }
+
+            return result;
+
+        }
+
+        private string EncodeBase64(byte[] inarr, out double informationContentBase64)
+        {
+            List<byte> arr = inarr.ToList();
+            string result = "";
+
+            while (arr.Count % 3 != 0)
+            {
+                arr.Add(0);
+            }
+
+            for (int i = 0; i < arr.Count; i += 3)
+            {
+                result += Encode3bytes(new byte[] { arr[i], arr[i + 1], arr[i + 2] });
+            }
+
+            // Обчислюємо кількість інформації в base64-закодованому варіанті
+            informationContentBase64 = result.Length * 8;
+
+            return result;
+        }
+
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    byte[] fileBytes = File.ReadAllBytes(openFileDialog1.FileName);
+
+                    // Отримуємо base64-кодований рядок та кількість інформації в base64-закодованому варіанті
+                    double informationContentBase64 = 0;
+                    string base64EncodedString = EncodeBase64(fileBytes, out informationContentBase64);
+
+                    // Кількість інформації в байтах (поділено на 8 бітів в байті)
+                    double informationContentBytes = informationContentBase64 / 8;
+
+                    // Відображаємо кількість інформації в байтах
+                    label4.Text = $"Кількість інформації в base64: {informationContentBytes} байт";
+
+                    // Відображаємо закодований рядок в richTextBox
+                    richTextBox2.Text = base64EncodedString;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Помилка: {ex.Message}");
+                }
+            }
+        }
+
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Base64 files (*.base64)|*.base64|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = saveFileDialog1.FileName;
+
+                    File.WriteAllText(filePath, richTextBox2.Text);
+
+                    MessageBox.Show("Файл успішно збережено.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Сталася помилка при збереженні файлу: {ex.Message}");
+                }
+            }
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
